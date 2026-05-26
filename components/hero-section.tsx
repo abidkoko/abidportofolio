6678f76c6585d133c18bad04f06199361000c32d
Abid Koko <btlabd52@gmail.com>
2026-05-26 14:11:38 +0700

    update

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { motion } from "framer-motion"
-import { Linkedin, Instagram, Download, ArrowRight, Mail } from "lucide-react"
+import { Linkedin, Instagram, MessageCircle, Download, ArrowRight, Mail } from "lucide-react"
 import { useEffect, useState } from "react"
 import Image from "next/image"
 
